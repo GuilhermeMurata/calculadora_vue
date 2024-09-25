20115fb22c93c8dd13f0718d6d8457c62ecfc676
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-09-25 19:04:42 -0300

    Entendendo webinar da calculadora

---
+++ webinar/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive } from 'vue';
+import header from './components/header.vue';
+
+  const estado = reactive({
+    operation: '+',
+    num1: '',
+    num2: ''
+  })
+
+  const resultado = ()=> {
+    const {operation} = estado
+    switch(operation) {
+      case'-':
+      return parseFloat(estado.num1) - parseFloat(estado.num2)
+      case'*':
+      return parseFloat(estado.num1) * parseFloat(estado.num2)
+      case'/':
+      return parseFloat(estado.num1) / parseFloat(estado.num2)
+      case'+':
+      return parseFloat(estado.num1) + parseFloat(estado.num2)
+    }
+  }
+</script>
---
+++ webinar/src/components/operacao.vue
@@ -0,0 +1,4 @@
+<script setup>
+    const props = defineProps(['operacao', 'num1', 'num2'])
+
+</script>
