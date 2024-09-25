76b6649ccfde2ea20aa03b92368a6da26c9e6ce9
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-09-25 19:37:48 -0300

    Verify

--- webinar/src/App.vue
+++ webinar/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { reactive } from 'vue';
-import header from './components/header.vue';
+import Cabecalho from './components/Cabecalho.vue'
+import Operacao from './components/Operacao.vue';
+import Resultado from './components/Resultado.vue';
 
   const estado = reactive({
     operation: '+',
@@ -22,3 +24,25 @@ import header from './components/header.vue';
     }
   }
 </script>
+
+<template>
+<body>
+  <div class="container">
+    <cabecalho/>
+    <Operacao :operacao="evento=> estado.operation = evento.target.value" :num1="evento => estado.num1 = evento.target.value" :num2="evento => estado.num2 = evento.target.value"></Operacao>
+    <Resultado :num1 ="estado.num1" :num2="estado.num2" :result="resultado()"/>
+  </div>
+</body>
+</template>
+<style scoped>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  .container{
+    margin: 0 auto;
+    text-align: center;
+    background-color: gray;
+  }
+</style>
